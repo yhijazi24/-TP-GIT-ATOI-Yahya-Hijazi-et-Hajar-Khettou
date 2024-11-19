66e370852ed8069774d7109487c5edf280cec420
yhijazi24 <yahyahijazi643@gmail.com>
2024-11-19 12:11:11 +0100

    On a supprimé les symboles HTML par leurs équivalents

--- mandel.c
+++ mandel.c
@@ -2,66 +2,66 @@
 #include <complex.h>
 #include <math.h>
 #include "ppm.h"
- 
- 
+
 #define TRSH 2.0
 #define ITER 1024ull
- 
+
 #define SIZEX 1500
 #define SIZEY 1500
- 
-double cx( int x )
+
+double cx(int x)
 {
     /* -2 ---> 1 */
     static const double qx = 3.0 / (double)SIZEX;
-    return -2.0 +  x * qx;
+    return -2.0 + x * qx;
 }
- 
-double cy( int y )
+
+double cy(int y)
 {
     /* -1 ---> 1 */
     static const double qy = 2.0 / (double)SIZEY;
     return -1.0 + y * qy;
 }
- 
+
 int main(int argc, char *argv[])
 {
     struct ppm_image im;
-    ppm_image_init( &amp;im , SIZEX , SIZEY );
- 
-    int i,j;
-    double colref = 255.0/log(ITER);
- 
+    /* Nous avons supprimé 'amp;' avant 'im' */
+    ppm_image_init(&im, SIZEX, SIZEY);
+
+    int i, j;
+    double colref = 255.0 / log(ITER);
+
     for (i = 0; i < SIZEX; ++i) {
         for (j = 0; j < SIZEY; ++j) {
- 
             unsigned long int iter = 0;
- 
-            double complex c =  cx(i) + cy(j) * I;
+
+            double complex c = cx(i) + cy(j) * I;
             double complex z = 0;
- 
-            while(iter < ITER)
-            {
+
+            while (iter < ITER) {
                 double mod = cabs(z);
- 
-                if( TRSH &lt; mod )
-                {
+                /* Nous avons supprimé '&lt;' car son équivalent est '<' */
+
+                if (TRSH < mod) {
                     break;
                 }
- 
-                z = z*z + c;
- 
+
+                z = z * z + c;
                 iter++;
             }
- 
-            int grey = colref*log(iter);
-            ppm_image_setpixel(&amp;im, i,j, grey, grey , grey );
+
+            int grey = colref * log(iter);
+            /* Nous avons supprimé 'amp;' avant 'im' */
+
+            ppm_image_setpixel(&im, i, j, grey, grey, grey);
         }
     }
- 
-    ppm_image_dump( &amp;im, "m.ppm");
-    ppm_image_release( &amp;im );
- 
- 
+    /* Nous avons supprimé 'amp;' avant 'im' */
+    ppm_image_dump(&im, "m.ppm");
+    /* Nous avons supprimé 'amp;' avant 'im' */
+
+    ppm_image_release(&im);
+
     return 0;
 }
